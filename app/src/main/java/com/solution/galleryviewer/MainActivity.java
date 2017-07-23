8829f6727cb7e0a6e5d8e7f888fa8ca3a8066886
Dmitriy Ogirenko <rangerover12@gmail.com>
2017-07-23 07:15:59 +0300

    - initialize project;

---
+++ app/src/main/java/com/solution/galleryviewer/MainActivity.java
@@ -0,0 +1,13 @@
+package com.solution.galleryviewer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
